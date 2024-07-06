801abd97d917d4598614fb859fd6f51f2af344b3
johnbrightagbons <brightinojohnny@gmail.com>
2024-07-06 11:57:22 +0100

    temple-filtered

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,136 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Update current year in the footer
+    const currentYearElement = document.getElementById('currentyear');
+    const currentYear = new Date().getFullYear();
+    currentYearElement.textContent = currentYear;
+
+    // Update last modified date in the footer
+    const lastModifiedElement = document.getElementById('lastModified');
+    const lastModified = document.lastModified;
+    lastModifiedElement.textContent = `Last Modified: ${lastModified}`;
+})
+
+    // Hamburger menu toggle functionality
+    const burger = document.querySelector('#burgerMenu')
+    const closeButton = document.querySelector('#closeButton')
+    const nav = document.querySelector('#menu')
+
+    burger.addEventListener('click', () => {
+    nav.classList.toggle('open')
+    closeButton.classList.toggle('show')
+})
+
+    closeButton.addEventListener('click', () => {
+    nav.classList.toggle('open')
+    closeButton.classList.toggle('show')
+})
+
+
+
+
+
+const temples = [
+    {
+      templeName: "Aba Nigeria",
+      location: "Aba, Nigeria",
+      dedicated: "2005, August, 7",
+      area: 11500,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+      templeName: "Manti Utah",
+      location: "Manti, Utah, United States",
+      dedicated: "1888, May, 21",
+      area: 74792,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+      templeName: "Payson Utah",
+      location: "Payson, Utah, United States",
+      dedicated: "2015, June, 7",
+      area: 96630,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+      templeName: "Yigo Guam",
+      location: "Yigo, Guam",
+      dedicated: "2020, May, 2",
+      area: 6861,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+      templeName: "Washington D.C.",
+      location: "Kensington, Maryland, United States",
+      dedicated: "1974, November, 19",
+      area: 156558,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+      templeName: "Lima Perú",
+      location: "Lima, Perú",
+      dedicated: "1986, January, 10",
+      area: 9600,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+      templeName: "Mexico City Mexico",
+      location: "Mexico City, Mexico",
+      dedicated: "1983, December, 2",
+      area: 116642,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+
+    // Add more temple objects here...
+    {
+        templeName: "Adelaide Australia Temple",
+        location: "Adelaide, Australia",
+        dedicated: "2000, June, 15 ",
+        area: 1.7,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/adelaide-australia/400x250/adelaide-australia-temple-lds-675332-wallpaper.jpg"
+      },
+
+      {
+        templeName: "Apia Samoa Temple",
+        location: "American Samoa",
+        dedicated: "2005, September, 4 ",
+        area: 2.6,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/apia-samoa/400x225/apia-samoa-temple-lds-495972-wallpaper.jpg"
+      },
+
+      {
+        templeName: "Accra Ghana Temple",
+        location: "Accra, Ghana",
+        dedicated: "2004, January, 11 ",
+        area: 6.75,
+        imageUrl:
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/400x225/accra-ghana-temple-detail-249022-2400x1200.jpg"
+      },
+  ];
+
+
+  // Display temples in the section
+  const templeList = document.getElementById('templeList');
+
+  temples.forEach(temple => {
+      const templeDiv = document.createElement('div');
+      templeDiv.classList.add('temple-card');
+
+      templeDiv.innerHTML = `
+          <h3>${temple.templeName}</h3>
+          <p><strong>Location:</strong> ${temple.location}</p>
+          <p><strong>Dedicated:</strong> ${temple.dedicated}</p>
+          <p><strong>Area:</strong> ${temple.area} sq ft</p>
+          <img src="${temple.imageUrl}" alt="${temple.templeName}" loading="lazy">
+      `;
+
+      templeList.appendChild(templeDiv);
+  });
