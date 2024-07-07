92a06bea1c56ff4a304ad2cf113d3edd6d7c6877
johnbrightagbons <brightinojohnny@gmail.com>
2024-07-07 03:44:07 +0100

    filtered temple

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -10,21 +10,22 @@ document.addEventListener("DOMContentLoaded", function() {
     lastModifiedElement.textContent = `Last Modified: ${lastModified}`;
 })
 
-    // Hamburger menu toggle functionality
-    const burger = document.querySelector('#burgerMenu')
-    const closeButton = document.querySelector('#closeButton')
-    const nav = document.querySelector('#menu')
+// Hamburger menu toggle functionality
+const burger = document.querySelector('#burgerMenu');
+const closeButton = document.querySelector('#closeButton');
+const nav = document.querySelector('#menu');
 
-    burger.addEventListener('click', () => {
-    nav.classList.toggle('open')
-    closeButton.classList.toggle('show')
-})
+burger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    closeButton.classList.toggle('show');
+    burger.classList.toggle('show');
+});
 
-    closeButton.addEventListener('click', () => {
-    nav.classList.toggle('open')
-    closeButton.classList.toggle('show')
-})
-
+closeButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    closeButton.classList.toggle('show');
+    burger.classList.toggle('show');
+});
 
 
 
