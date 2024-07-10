9a79459cf5b08a9df410ab7ea9f90c4cb2ae3edd
johnbrightagbons <brightinojohnny@gmail.com>
2024-07-10 20:33:29 +0100

    siteplan

---
+++ week03/scripts/objects-rep.js
@@ -0,0 +1,16 @@
+// courses.js
+const aCourse = {
+    code: "CSE121b",
+    name: "Javascript Language",
+  };
+
+
+ // Activity 1 #
+// Use objects and encapsulation to create a representation of a school schedule
+// Review the object literal you were given represent a course. It includes the course name and course number.
+
+const course = {
+    { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+{ sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+
+}
